c550117b12871520f94b9a37a90e3c4c8fe04cf4
adriandpdev <adri.2000@hotmail.com>
2020-11-12 21:22:59 +0100

    login

---
+++ src/Ac_Login.java
@@ -0,0 +1,15 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ac_Login implements ActionListener {
+	public V_Login vent;
+	Ac_Login (V_Login v){
+		vent = v;
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/V_Login.java
+++ src/V_Login.java
@@ -1,7 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 public class V_Login extends JFrame{
+	private JTextField txt[] = new JTextField[2];
+	private JLabel lbl[] = new JLabel[3];
+	private JCheckBox ch;
+	private JButton btn;
+	
 	V_Login() {
 		super("Inicio de sesión");
 		try {
@@ -13,7 +25,25 @@ public class V_Login extends JFrame{
 		this.setLocation(100, 100); // PONER AL CENTRO
 		this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE); 
 		this.setResizable(false); 
-		this.setLayout(null);
+		this.setLayout(new GridLayout(4,2,50,50));
+		
+		txt[0] = new JTextField();
+		txt[1] = new JTextField();
+		lbl[0] = new JLabel("DNI");
+		lbl[1] = new JLabel("Contraseña");
+		lbl[2] = new JLabel("He olvidado mi contraseña");
+		btn = new JButton("Iniciar sesión");
+		btn.addActionListener(new Ac_Login(this));
+		ch = new JCheckBox("Recordar DNI");
+		
+		this.getContentPane().add(lbl[0]);
+		this.getContentPane().add(txt[0]);
+		this.getContentPane().add(lbl[1]);
+		this.getContentPane().add(txt[1]);
+		this.getContentPane().add(ch);
+		this.getContentPane().add(btn);
+		this.getContentPane().add(lbl[2]);
+		
 		
 		this.setVisible(true);
 	}
