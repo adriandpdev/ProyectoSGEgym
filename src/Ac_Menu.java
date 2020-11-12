1996f205dae516982f1000d2a9243ec66c519702
adriandpdev <adri.2000@hotmail.com>
2020-11-12 17:47:35 +0100

    Menu v2.0

--- src/Ac_Menu.java
+++ src/Ac_Menu.java
@@ -1,5 +1,8 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.*;
 
 public class Ac_Menu implements ActionListener {
 	private V_AdminHome vent;
@@ -10,22 +13,53 @@ public class Ac_Menu implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if (arg0.getActionCommand().equals("Usuarios")) {
+		if (arg0.getActionCommand().equals("Alta usuario")) {
 			V_AdminUserAdd vAdUsAdd = new V_AdminUserAdd();
 			//vAdUsAdd.setVisible(true);
-		} else if (arg0.getActionCommand().equals("Profesores")) {
-			V_AdminHome vNum = new V_AdminHome();
-			vNum.setVisible(true);
-		} else if (arg0.getActionCommand().equals("Clases")) {
-			vent.dispose();
-		} else if (arg0.getActionCommand().equals("Horarios")) {
-			V_AdminHome vNum = new V_AdminHome();
-			vNum.setVisible(true);
-		} else if (arg0.getActionCommand().equals("Newslater")) {
-			vent.dispose();
-		} else if (arg0.getActionCommand().equals("Facturas")) {
-			V_AdminHome vNum = new V_AdminHome();
-			vNum.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Lista usuarios")) {
+			V_AdminUserList vAdUsList = new V_AdminUserList();
+			//vAdUsList.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Pendiente de pago")) {
+			V_AdminUserPend vAdUsPend = new V_AdminUserPend();
+			//vAdUsPend.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Alta profesor")) {
+			V_AdminEmplAdd vAdEmAdd = new V_AdminEmplAdd();
+			//vAdEmAdd.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Lista profesores")) {
+			V_AdminEmplList vAdEmList = new V_AdminEmplList();
+			//vAdEmList.setVisible(true);
+		}else if (arg0.getActionCommand().equals("Alta actividad")) {
+			V_AdminActiAdd vAdAcAdd = new V_AdminActiAdd();
+			//vAdAcAdd.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Lista actividades")) {
+			V_AdminActiList vAdAcList = new V_AdminActiList();
+			//vAdAcList.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Añadir hora")) {
+			V_AdminScheAdd vAdScAdd = new V_AdminScheAdd();
+			//vAdScAdd.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Visualizar horario")) {
+			V_AdminScheList vAdScList = new V_AdminScheList();
+			//vAdScList.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Exportar PDF")) {
+			V_AdminScheExp vAdScExp = new V_AdminScheExp();
+			//vAdScExp.setVisible(true);
+		}else if (arg0.getActionCommand().equals("Promociones")) {
+			V_AdminProm vAdProm = new V_AdminProm();
+			//vAdProm.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Avisos")) {
+			V_AdminWarn vAdWarn = new V_AdminWarn();
+			//vAdWarn.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Pagos usuarios")) {
+			V_AdminPayUs vAdPayUs = new V_AdminPayUs();
+			//vAdPayUs.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Nominas profesores")) {
+			V_AdminPayEm vAdPayEm = new V_AdminPayEm();
+			//vAdPayEm.setVisible(true);
+		} else if (arg0.getActionCommand().equals("Cerrar ventana")) {
+			vent.removeAll();
+		}else if (arg0.getActionCommand().equals("Salir")) {
+			// Pedir confirmación
+			System.exit(0);
 		}
 	}
 }
--- src/V_AdminHome.java
+++ src/V_AdminHome.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class V_AdminHome extends JFrame{
 	private JMenuBar jmb;
-	private JMenu me;
-	private JMenuItem[] mi = new JMenuItem[3];
+	private JMenu[] me = new JMenu[7];
+	private JMenuItem[] mi = new JMenuItem[16];
 	private JSeparator sep;
 	
 	V_AdminHome() {
@@ -12,24 +12,88 @@ public class V_AdminHome extends JFrame{
 		this.setLocation(100, 100); // PONER AL CENTRO
 		this.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE); 
 		this.setResizable(false); 
-		this.setLayout(null);
 		
 		jmb = new JMenuBar();
-		this.getContentPane().add(jmb);
-		me = new JMenu("MENU DE OPCIONES");
-		jmb.add(me);
-		mi[0]= new JMenuItem("Piedra, papel o Tijera");
+		this.setJMenuBar(jmb);
+		
+		me[0] = new JMenu("Usuarios");
+		jmb.add(me[0]);
+		mi[0]= new JMenuItem("Alta usuario");
 		mi[0].addActionListener(new Ac_Menu(this));
-		me.add(mi[0]);
-		mi[1]= new JMenuItem("Números");
+		me[0].add(mi[0]);
+		mi[1]= new JMenuItem("Lista usuarios");
 		mi[1].addActionListener(new Ac_Menu(this));
-		me.add(mi[1]);
-		sep = new JSeparator();
-		me.add(sep);
-		mi[2]= new JMenuItem("Salir");
+		me[0].add(mi[1]);
+		mi[2]= new JMenuItem("Pendiente de pago");
 		mi[2].addActionListener(new Ac_Menu(this));
-		me.add(mi[2]);
+		me[0].add(mi[2]);
+		
+		me[1] = new JMenu("Profesores");
+		jmb.add(me[1]);
+		mi[3]= new JMenuItem("Alta profesor");
+		mi[3].addActionListener(new Ac_Menu(this));
+		me[1].add(mi[3]);
+		mi[4]= new JMenuItem("Lista profesores");
+		mi[4].addActionListener(new Ac_Menu(this));
+		me[1].add(mi[4]);
+	
+		me[2] = new JMenu("Actividades");
+		jmb.add(me[2]);
+		mi[5]= new JMenuItem("Alta actividad");
+		mi[5].addActionListener(new Ac_Menu(this));
+		me[2].add(mi[5]);
+		mi[6]= new JMenuItem("Lista actividades");
+		mi[6].addActionListener(new Ac_Menu(this));
+		me[2].add(mi[6]);
+
+		
+		me[3] = new JMenu("Horario");
+		jmb.add(me[3]);
+		mi[7]= new JMenuItem("Añadir hora");
+		mi[7].addActionListener(new Ac_Menu(this));
+		me[3].add(mi[7]);
+		mi[8]= new JMenuItem("Visualizar horario");
+		mi[8].addActionListener(new Ac_Menu(this));
+		me[3].add(mi[8]);
+		mi[9]= new JMenuItem("Exportar PDF");
+		mi[9].addActionListener(new Ac_Menu(this));
+		me[3].add(mi[9]);
+		
+		me[4] = new JMenu("Newsletter");
+		jmb.add(me[4]);
+		mi[10]= new JMenuItem("Promociones");
+		mi[10].addActionListener(new Ac_Menu(this));
+		me[4].add(mi[10]);
+		mi[11]= new JMenuItem("Avisos");
+		mi[11].addActionListener(new Ac_Menu(this));
+		me[4].add(mi[11]);
+		
+		me[5] = new JMenu("Facturas");
+		jmb.add(me[5]);
+		mi[12]= new JMenuItem("Pagos usuarios");
+		mi[12].addActionListener(new Ac_Menu(this));
+		me[5].add(mi[12]);
+		mi[13]= new JMenuItem("Nominas profesores");
+		mi[13].addActionListener(new Ac_Menu(this));
+		me[5].add(mi[13]);
+		
+		me[6] = new JMenu("Opciones");
+		jmb.add(me[6]);
+		mi[14]= new JMenuItem("Cerrar ventana");
+		mi[14].addActionListener(new Ac_Menu(this));
+		me[6].add(mi[14]);
+		mi[15]= new JMenuItem("Salir");
+		mi[15].addActionListener(new Ac_Menu(this));
+		me[6].add(mi[15]);
 		
 		this.setVisible(true);
 	}
 }
+
+
+
+
+
+
+
+
