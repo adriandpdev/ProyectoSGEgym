6c1be4e5c0d8f9e20bde23d8b54c7ef6fdfd55f1
ZamarroLopez <mariolopezelices@gmail.com>
2020-11-18 13:40:07 +0100

    Funcional de el cambio de contraseña de usuario.

--- src/UserBack/Ac_UserPerfil.java
+++ src/UserBack/Ac_UserPerfil.java
@@ -1,7 +1,11 @@
 package UserBack;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
 import javax.swing.JOptionPane;
+
+import com.mysql.cj.protocol.Resultset;
 
 import UserFront.V_UserPerfil;
 import main.Conexion;
@@ -20,16 +24,26 @@ public class Ac_UserPerfil implements ActionListener {
 			try {
 				c.alta(Main.con, "UPDATE Persona SET nombre = '"+vent.getTxt_Nombre().getText()+"', apellido = '"+vent.getTxt_Apell().getText()+"', cuentabanc = '"+vent.getTxt_Cb().getText()+"', fechanac = '"+vent.getTxt_Nac().getText()+"', telefono = '"+Integer.parseInt(vent.getTxt_Tlf().getText())+"', correo = '"+vent.getTxt_Correo().getText()+"' WHERE DNI = '"+vent.getTxt_Dni().getText()+"'");
 				JOptionPane.showMessageDialog(vent, "Cambios guardados con exito!");
-				c.con.close();
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
 		}else if(e.getSource() == vent.getBtn_CambiarContraseña()) {
 			Conexion c = new Conexion();
+			String AntiguaContraseña = vent.getTxt_Antigua_Contraseña().getText();
+			String NuevaContraseña = vent.getTxt_Nueva_Contraseña().getText();
+			String pass = "";
 			try {
-				//c.alta(Main.con, "UPDATE Persona SET DNI = '"+vent.getTxt_Dni().getText()+"', nombre = '"+vent.getTxt_Nombre().getText()+"', apellido = '"+vent.getTxt_Apell().getText()+"', cuentabanc = '"+vent.getTxt_Cb().getText()+"', fechanac = '"+vent.getTxt_Nac().getText()+"', telefono = '"+Integer.parseInt(vent.getTxt_Tlf().getText())+"', correo = '"+vent.getTxt_Correo().getText()+"' WHERE DNI = '"+vent.getTxt_Dni().getText()+"'");
-				JOptionPane.showMessageDialog(vent, "Contraseña cambiada con Exito!");
-				c.con.close();
+				//Almaceno la contraseña de la persona con el DNI logeado.
+				ResultSet rs = c.consulta(Main.con, "SELECT pass FROM Persona WHERE DNI ='"+vent.getTxt_Dni().getText()+"'");
+				while (rs.next()) {
+					pass = rs.getString("pass");
+				}
+				if(AntiguaContraseña.isEmpty()||NuevaContraseña.isEmpty()) {
+					JOptionPane.showMessageDialog(vent, "Inserte la contraseña antigua y la nueva.");
+				}else if(AntiguaContraseña.equals(pass)) {
+					c.alta(Main.con, "UPDATE Persona SET pass = '"+NuevaContraseña+"' WHERE DNI = '"+vent.getTxt_Dni().getText()+"'");
+					JOptionPane.showMessageDialog(vent, "Contraseña cambiada con Exito!");
+				}
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
