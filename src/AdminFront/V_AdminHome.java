cfcb6907671ec152b4746576d1f91547bef4ff66
PBernalM-S <pablobern3110@gmail.com>
2020-11-19 20:51:09 +0100

    Nueva libreria y cambios home

--- src/AdminFront/V_AdminHome.java
+++ src/AdminFront/V_AdminHome.java
@@ -3,6 +3,7 @@ package AdminFront;
 import javax.swing.*;
 import javax.swing.UIManager.*;
 
+import AdminBack.Ac_AdminScheExp;
 import AdminBack.Ac_MenuAdmin;
 
 public class V_AdminHome extends JFrame {
@@ -62,7 +63,7 @@ public class V_AdminHome extends JFrame {
 		mi[8].addActionListener(new Ac_MenuAdmin(this));
 		me[3].add(mi[8]);
 		mi[9] = new JMenuItem("Exportar PDF");
-		mi[9].addActionListener(new Ac_MenuAdmin(this));
+		mi[9].addActionListener(new Ac_AdminScheExp());
 		me[3].add(mi[9]);
 
 		me[4] = new JMenu("Newsletter");
