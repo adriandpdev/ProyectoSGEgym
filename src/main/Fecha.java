950f373225c885a4ae05136c5f9dfb8226bc5e1c
RaulVidal58 <rvidalr.salesianospizarrales.educamos.com>
2020-11-24 21:20:29 +0100

    Clase que devuelve la fecha del sistema

---
+++ src/main/Fecha.java
@@ -0,0 +1,48 @@
+package main;
+
+import java.util.Calendar;
+
+public class Fecha {
+	
+	Calendar fecha = Calendar.getInstance();
+	
+	int dia = fecha.get(Calendar.DAY_OF_MONTH);
+	int mes = fecha.get(Calendar.MONTH)+1;
+	int año = fecha.get(Calendar.YEAR);
+	
+	
+	
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAño() {
+		return año;
+	}
+
+	public void setAño(int año) {
+		this.año = año;
+	}
+
+
+
+	public String fechaActual()
+	{
+		String fecha = dia+"/"+mes+"/"+año;
+		
+		return fecha;
+	}
+
+}
