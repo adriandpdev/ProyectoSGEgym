02bca1ecdaf785305563d743524ecb97b0eb3280
Echomarr <55403174+Echomarr@users.noreply.github.com>
2020-11-30 00:08:03 +0100

    Add files via upload

--- src/listado/PanelModificacion.java
+++ src/listado/PanelModificacion.java
@@ -3,23 +3,28 @@ package listado;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import javax.swing.*;
 
 import com.toedter.calendar.JDateChooser;
 
-public class PanelModificacion extends JFrame{
+public class PanelModificacion extends JFrame implements ActionListener{
 
 	private JLabel Titulo, Dni, Nombre, CCC, email, Fecha, Apellidos, Contraseña, Telefono, Rol;
 	private JTextField txtDni, txtNombre, txtCCC, txtemail, txtApellidos, txtContraseña, txtTelefono, txtRol;
 	private JFormattedTextField txtfecha;
-	private JButton Añadir, Limpiar;
-	private JDateChooser date;
+	private JButton Modificar;
+	private JDateChooser Calendario;
+	private String DNI;
 	
 	private Conexion cp;
 	private Connection conn;
@@ -59,7 +64,9 @@ public class PanelModificacion extends JFrame{
 
 	//Construtor sobrecargado con todos los datos de la BBDD
 	
-	public PanelModificacion(String dni, String nombre, String apellido, String cuentabanc, String pass, String fechanac, String telefono, String correo, String rol) {
+	public PanelModificacion(String dni, String nombre, String apellido, String cuentabanc, String pass, String fechanac, String telefono, String correo, String rol) throws ParseException {
+		
+		this.DNI = dni;
 		CreateForm();
 		FillFields(dni, nombre, apellido, cuentabanc, pass, fechanac, telefono, correo, rol);
 	}
@@ -83,10 +90,10 @@ public class PanelModificacion extends JFrame{
 		Centro.add(txtCCC = new JTextField());
 		Centro.add(email = new JLabel("E-mail"));
 		Centro.add(txtemail = new JTextField());
-		date = new JDateChooser();
+		Calendario = new JDateChooser();
 		Centro.add(Fecha = new JLabel("Fecha"));
-		Centro.add(date);
-		System.out.println(date);
+		Centro.add(Calendario);
+		
 		Centro.add(Contraseña = new JLabel("Contraseña"));
 		Centro.add(txtContraseña = new JTextField());
 		Centro.add(Telefono = new JLabel("Telefono"));
@@ -97,8 +104,8 @@ public class PanelModificacion extends JFrame{
 		txtContraseña.setEnabled(false);
 		txtContraseña.setText(getPassword());
 		JPanel Sur = new JPanel();
-		Sur.add(Añadir = new JButton("Modificar"));
-		//Añadir.addActionListener(new Ac_AdminEmplAdd(this));
+		Sur.add(Modificar = new JButton("Modificar"));
+		Modificar.addActionListener(this);
 		
 
 		this.getContentPane().add(Norte, BorderLayout.NORTH);
@@ -110,11 +117,11 @@ public class PanelModificacion extends JFrame{
 	}
 
 	public JDateChooser getDate() {
-		return date;
+		return Calendario;
 	}
 
 	public void setDate(JDateChooser date) {
-		this.date = date;
+		this.Calendario = date;
 	}
 
 	public JTextField getTxtDni() {
@@ -181,17 +188,17 @@ public class PanelModificacion extends JFrame{
 		this.txtfecha = txtfecha;
 	}
 
-	private void FillFields(String dni, String nombre, String apellido, String cuentabanc, String pass, String fechanac, String telefono, String correo, String rol) {
+	private void FillFields(String dni, String nombre, String apellido, String cuentabanc, String pass, String fechanac, String telefono, String correo, String rol) throws ParseException {
 		// Metodo para rellenar los campos con la información de la BD
 		
-		//Date fech = new SimpleDateFormat("dd-MM-yyyy").parse(fechanac);
+		java.util.Date fech_parse = new SimpleDateFormat("dd-MM-yyyy").parse(fechanac);
 		
 		txtDni.setText(dni);
 		txtNombre.setText(nombre);
 		txtApellidos.setText(apellido);
 		txtCCC.setText(cuentabanc);
 		txtContraseña.setText(pass);
-		//date.setDate(fech);
+		Calendario.setDate(fech_parse);
 		txtTelefono.setText(telefono);
 		txtemail.setText(correo);
 		txtRol.setText(rol);
@@ -200,17 +207,45 @@ public class PanelModificacion extends JFrame{
 
 	}
 	
-	public int modificarDatos(String dni, String nombre, String apellido, String cuentabanc, String pass, String fechanac, String telefono, String correo, String rol) {
+	public void modificarDatos() {
 		
-		int tele = Integer.valueOf(telefono);
+		//Cogemos los nuevos datos del panel de modificaciones
 		
-		String sql_modificar = "UPDATE persona SET dni='"+dni+"', apellido= '"+apellido+"',cuentabanc='"+cuentabanc+"',pass='"+pass+"', fechanac='"+fechanac+"',"
-				+ "telefono=" +tele+ " correo='"+correo+"', rol='"+rol+"' WHERE dni='"+dni+"'";
+		String mod_dni = txtDni.getText().toString();
+		String mod_nombre = txtNombre.getText().toString();
+		String mod_apellido = txtApellidos.getText().toString();
+		String mod_cuentabanc = txtCCC.getText().toString();
+		String tele = txtTelefono.getText().toString();
+		String mod_correo = txtemail.getText().toString();
+		String mod_rol = txtRol.getText().toString();
 		
-		int res = 0;
 		
+		//Parseamos el teléfono a un integer para que no dé fallo al modificar la BBDD
+		int mod_telefono = Integer.valueOf(tele);
+		
+		//Parseamos la fecha en formato date a string dd-MM-yyyy
+		String pattern = "dd-MM-yyyy";
+		DateFormat df = new SimpleDateFormat(pattern);    
+		String mod_fechanac = df.format(Calendario.getDate());
+		
+		
+		System.out.println(DNI);
+		System.out.println(mod_dni);
+		System.out.println(mod_nombre);
+		System.out.println(mod_apellido);
+		System.out.println(mod_cuentabanc);
+		System.out.println(mod_fechanac);
+		System.out.println(mod_telefono);
+		System.out.println(mod_correo);
+		System.out.println(mod_rol);
+		
+		String sql_modificar = "UPDATE persona SET dni='"+mod_dni+"', nombre='"+mod_nombre+"', apellido='"+mod_apellido+"',cuentabanc='"+mod_cuentabanc+"', fechanac='"+mod_fechanac+"',"
+			+ "telefono="+mod_telefono+", correo='"+mod_correo+"', rol='"+mod_rol+"' WHERE dni='"+DNI+"'";
+
+		
+		//UPDATE `persona` SET `fechanac`= "18-01-1980" WHERE 1
+				
 		cp = new Conexion();
-		
 		
 		try {
 			conn = cp.conectar();
@@ -219,21 +254,31 @@ public class PanelModificacion extends JFrame{
 			e.printStackTrace();
 		}
 		try {
-			rs = cp.consulta(conn, sql_modificar);
+			cp.modificar(conn, sql_modificar);
+			JOptionPane.showMessageDialog(null, "El registro ha sido modificado correctamente");
+			dispose();
 			
-			if (res > 0) {
-				JOptionPane.showConfirmDialog(null, "El registro ha sido modificado correctamente");
-			}
+			listado list = new listado();
+			list.refrescarTabla();
 			
 			conn.close();
-			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			JOptionPane.showConfirmDialog(null, "Error al modificar el registro");
+			JOptionPane.showMessageDialog(null, "No se han podido realizar los cambios");
 		}
 		
-		return res;
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Pulsado modificar");
+		if(e.getSource() == Modificar) {
+			modificarDatos();
+		}
+		
 	}
 }
