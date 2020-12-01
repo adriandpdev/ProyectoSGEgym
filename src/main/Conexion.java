f585610782a4fb5da7292800d4b10fa936b5e44c
adriandpdev <adri.2000@hotmail.com>
2020-12-01 20:41:26 +0100

    hotfix conexion

--- src/main/Conexion.java
+++ src/main/Conexion.java
@@ -1,6 +1,7 @@
-package listado;
+package main;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Conexion {
 	public Connection con;
@@ -36,5 +37,46 @@ public class Conexion {
 	public void modificar(Connection con, String query) throws SQLException {
 		Statement stm = con.createStatement();
 		stm.executeUpdate(query);
+	}
+	public ArrayList<String> llenarCombo(Connection con, String q, String x) throws SQLException
+	{
+		Statement stm = con.createStatement();
+		ResultSet rs = null;
+
+		ArrayList<String> lista = new ArrayList<String>();
+
+		try {
+			rs = stm.executeQuery(q);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		try {
+			while(rs.next())
+			{
+				lista.add(rs.getString(x));
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return lista;
+	}
+
+	public boolean comprobarId(Connection con, String id) throws SQLException
+	{
+		boolean flag = false;
+
+		Statement stm = con.createStatement();
+		ResultSet rs = stm.executeQuery("SELECT idActividad FROM Actividad");
+
+		while(rs.next())
+		{
+			if(rs.getString("idActividad").equals(id))
+			{
+				flag = true;
+			}
+		}
+
+		return flag;
 	}
 }
