9a4803691afa5066320365f4d73bfa36d9c59cae
Aredondom <aredondom@salesianospizarrales.educamos.com>
2020-12-03 12:39:15 +0100

    Arreglado mostrar datos avisos y promociones

--- src/UserFront/promocionesJtableModel.java
+++ src/UserFront/promocionesJtableModel.java
@@ -4,20 +4,19 @@ import java.util.ArrayList;
 
 import javax.swing.table.AbstractTableModel;
 
-import com.sun.tools.javac.util.List;
 
 public class promocionesJtableModel extends AbstractTableModel {
  ArrayList<String[]> promociones = new ArrayList<String[]>();
 	@Override
 	public int getRowCount() {
 		// TODO Auto-generated method stub
-		return 3;
+		return promociones.size();
 	}
 
 	@Override
 	public int getColumnCount() {
 		// TODO Auto-generated method stub
-		return promociones.size();
+		return 3;
 	}
 
 	@Override
